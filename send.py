d6ac5f7c99a954e970a856f79a9f34e1c9042341
Isanur960 <41563572+Isanur960@users.noreply.github.com>
2020-06-15 20:43:05 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,11 +1,11 @@
-
 import smtplib
-fromaddr = input('Input your Gmail address: ')
-toaddrs  = input('Input recipient email address: ')
-msg = input('Write the msg: ')
-username = formaddr
-password = input('Input Password')
+fromaddr = input('your email: ')
+toaddrs = input('Recippent email: ')
+msg = input('Msg: ')
+username = fromaddr
+password =input('Password: ')
 server = smtplib.SMTP('smtp.gmail.com:587')
+server.set_debuglevel(1)
 server.starttls()
 server.login(username,password)
 server.sendmail(fromaddr, toaddrs, msg)
